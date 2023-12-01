55b72121e74cd086eec6c4198969497d0a420fd4
Bas Jansen <basj9559@gmail.com>
2023-12-01 09:01:29 +0100

    Day 1 brushed up and added documentation

--- day1/part1.py
+++ day1/part1.py
@@ -1,36 +1,52 @@
-nums = {'one' : '1','two': '2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
-reversed = {'eno' : '1','owt': '2','eerht':'3','ruof':'4','evif':'5','xis':'6','neves':'7','thgie':'8','enin':'9'}
+number_dictionary = {'one' : '1','two': '2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+reversed_number_dictionary = {'eno' : '1','owt': '2','eerht':'3','ruof':'4','evif':'5','xis':'6','neves':'7','thgie':'8','enin':'9'}
 
 def main1():
-    lst = inputAsList()
+    input = inputAsList()
     sum = 0
-    for str in lst:
-        ints = ''.join(x for x in str if x.isdigit())
-        sum += int(ints[0] + ints[len(ints)-1])
+    for line in input:
+        # Get all integers in the string and put them in a list.
+        integer_list = ''.join(x for x in line if x.isdigit())
+
+        # Add the concatenated value of the first and last integer to the sum.
+        sum += int(integer_list[0] + integer_list[len(integer_list)-1])
     return sum
 
 def main2():
-    lst = inputAsList()
+    input = inputAsList()
     sum = 0
-    for str in lst:
-        first = getFirstNumber(str,nums)
-        last = getFirstNumber(str[::-1],reversed)
+    for line in input:
+
+        # Get the first and last number from the list.
+        first = getFirstNumber(line)
+        last = getLastNumber(line)
+
+        # Finally, the values are concatenated and summed.
         sum += int(''.join([first,last]))
     return sum
 
-def getFirstNumber(string, number_dictionary):
-    first = 0
-    while(first == 0 and len(string) != 0):
+# Retrieves the first number of a given string, which can either be a literal, or fully spelled out number.
+# The dictionary provides the mapping of the numbers written as strings, mapped onto their literal value.
+def getFirstNumber(string, number_dict=number_dictionary):
+    while(len(string) != 0):
+        # If the string starts with a digit, that digit is returned.
         if(string[0].isdigit()):
-            first = string[0]
-        else:
-            for key,value in number_dictionary.items():
-                if string.startswith(key):
-                    first = value
-                    break
-            string = string[1:]
-    return first
+            return string[0]
 
+        # If the string starts with a number written out, the corresponding value is returned.
+        else:
+            for key,value in number_dict.items():
+                if string.startswith(key):
+                    return value
+
+        # Repeat by removing the first character until a value is found.
+        string = string[1:]
+    return None
+
+# Retrieves the last number from a list, which could be a literal digit or fully spelled out number.
+def getLastNumber(line):
+    # getFirstNumber is called, but both the line and dictionary are reversed, resulting in the last number of the line.
+    return getFirstNumber(line[::-1], reversed_number_dictionary)
 
 
 def inputAsList():
