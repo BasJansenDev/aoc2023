99c6141271cf87c881cc5eb442636c6a236e7a7a
Bas Jansen <basj9559@gmail.com>
2023-12-01 11:29:13 +0100

    Day 1 Refactoring to merge part 1 and part 2 into one function

--- day1/part1.py
+++ day1/part1.py
@@ -1,53 +1,33 @@
 number_dictionary = {'one' : '1','two': '2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
 
-def main1():
+def main(part2):
     input = input_as_list()
     sum = 0
     for line in input:
-        # Get all integers in the string and put them in a list.
-        numbers = ''.join(x for x in line if x.isdigit())
+        number_list = []
+        while(len(line) != 0):
+            # If the line currently starts with a digit, that digit is added to the number list.
+            if(line[0].isdigit()):
+                number_list += line[0]
 
-        # Add the concatenated value of the first and last integer to the sum.
-        sum += int(numbers[0] + numbers[-1])
+            # Only for part two:
+            # If the line currently starts with a number written out, the corresponding value is added to the number list.
+            elif(part2):
+                for key,value in number_dictionary.items():
+                    if line.startswith(key):
+                        number_list += value
+                        break
+
+            # Repeat by removing the first character until no characters are left.
+            line = line[1:]
+
+        # Finally, the first and last number from the current line are concatenated and added to the total sum.
+        sum += int(''.join([number_list[0],number_list[-1]]))
     return sum
-
-def main2():
-    input = input_as_list()
-    sum = 0
-    for line in input:
-
-        # Get all the numbers from the list.
-        numbers = get_numbers(line)
-
-        # Add the concatenated first and last numbers to the sum
-        sum += int(''.join([numbers[0],numbers[-1]]))
-    return sum
-
-# Retrieves the first number of a given string, which can either be a literal, or fully spelled out number.
-# The dictionary provides the mapping of the numbers written as strings, mapped onto their literal value.
-def get_numbers(string):
-    number_list = []
-    while(len(string) != 0):
-        # If the string starts with a digit, that digit is added to the number list.
-        if(string[0].isdigit()):
-            number_list += string[0]
-
-        # If the string starts with a number written out, the corresponding value is added to the number list.
-        else:
-            for key,value in number_dictionary.items():
-                if string.startswith(key):
-                    number_list += value
-                    break
-
-        # Repeat by removing the first character until a value is found.
-        string = string[1:]
-    return number_list
-
 
 def input_as_list():
     f = open('input')
     return list(f.read().split('\n'))
 
-
-print(main1())
-print(main2())
+print(main(False))
+print(main(True))
